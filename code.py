5b1c825cf2ae5d53e91cafc10cd59b38aa1a364f
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-12-11 16:15:32 +0400

    add code

--- code.py
+++ code.py
@@ -1,1 +1,26 @@
 
+
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+''' recording the sound '''
+
+with sr.Microphone() as source:
+    print("Adjusting noise ")
+    recognizer.adjust_for_ambient_noise(source, duration=1)
+    print("Recording for 4 seconds")
+    recorded_audio = recognizer.listen(source, timeout=4)
+    print("Done recording")
+
+''' Recorgnizing the Audio '''
+try:
+    print("Recognizing the text")
+    text = recognizer.recognize_google(
+            recorded_audio, 
+            language="en-US"
+        )
+    print("Decoded Text : {}".format(text))
+
+except Exception as ex:
+    print(ex)
